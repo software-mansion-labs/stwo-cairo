47567e05410839c4ec678749284be44d9ae5f83c
Maciej Trątnowiecki <maciej.tratnowiecki@swmansion.com>
2025-11-27 12:39:33 +0100

    add prover feature to commons

--- stwo_cairo_prover/crates/common/src/lib.rs
+++ stwo_cairo_prover/crates/common/src/lib.rs
@@ -1,5 +1,7 @@
 #![cfg_attr(feature = "prover", feature(portable_simd))]
 pub mod builtins;
 pub mod memory;
-pub mod preprocessed_columns;
 pub mod prover_types;
+
+#[cfg(feature = "prover")]
+pub mod preprocessed_columns;
